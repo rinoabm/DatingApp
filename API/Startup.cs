8dce6d81006af04dfca128566ba7b87519272ba9
Abraham <rino.abraham@siemens.com>
2020-11-06 06:33:31 +0530

    End Of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,7 +36,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-
+            services.AddCors();
 
             services.AddSwaggerGen(c =>
             {
@@ -57,6 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4501"));
 
             app.UseAuthorization();
 
