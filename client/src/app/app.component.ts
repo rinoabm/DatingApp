db2cbcdab8ba24577020b2922f61b3761c70c762
Abraham <rino.abraham@siemens.com>
2020-11-20 12:26:24 +0530

    end of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users : any;
 
-  constructor(private http : HttpClient, private accountService: AccountService) {
+  constructor(private accountService: AccountService) {
 
   }
 
@@ -23,15 +23,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response =>{
-      this.users=response;
-    },error => {
-      console.log(error);
-    })
-  }
 }
